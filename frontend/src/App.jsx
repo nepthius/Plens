a8264f234262ae94198c6a5cfeba0beaf64a37a5
nepthius <anishdshekar@gmail.com>
2025-03-15 12:45:26 -0400

    add frontend changes to sync with backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,22 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-    const [message, setMessage] = useState("");
+    const [riskFactors, setRiskFactors] = useState([]);
 
     useEffect(() => {
         axios.get("/api/risk_factors")
-            .then((response) => setMessage(response.data))
+            .then((response) => setRiskFactors(response.data.riskFactors))
             .catch((error) => console.error("Error fetching data:", error));
     }, []);
 
     return (
         <div>
-            <h1>REACTTTTTTTTTTT</h1>
-            <p>Backend having a funny thing to say: {message}</p>
+            <h1>Health Risk Factors</h1>
+            <ul>
+                {riskFactors.map((factor, index) => (
+                    <li key={index}>{factor}</li>
+                ))}
+            </ul>
         </div>
     );
 }
