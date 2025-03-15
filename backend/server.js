562b75d75cdab39a5e0cf8b194b09d1d3eac085d
nepthius <anishdshekar@gmail.com>
2025-03-15 12:45:02 -0400

    tune front and backend functionality to be able to communicate with one another

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+app.use(cors());
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Howdy Plens!");
+});
+
+app.get("/api/risk_factors", (req, res) => {
+    res.json({
+        riskFactors: ["Facemaks A'pieu is high risk!", "Facemask Bertho is low risk...", "Facemask Ableu is Medium Risk."],
+    });
 });
 
 const PORT = process.env.PORT || 3000;
